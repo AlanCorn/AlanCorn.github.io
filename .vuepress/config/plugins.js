7e2ace5c4f2eca1185eb339f655901569de533a6
AlanCorn_02 <1561206831@qq.com>
2021-12-04 21:48:29 +0800

    :wrench:add cursor-effects

--- .vuepress/config/plugins.js
+++ .vuepress/config/plugins.js
@@ -2,18 +2,16 @@
 module.exports = [
   ['dynamic-title', {
     showIcon: '/favicon.ico',
-    showText: '(/≧▽≦/)咦！又好了！',
-    hideIcon: '/failure.ico',
-    hideText: '(●—●)喔哟，崩溃啦！',
+    showText: '(｡･∀･)ﾉﾞAlanCorn\'s Blog',
+    hideIcon: '/favicon.ico',
+    hideText: '(´･_･`)AlanCorn\'s Blog',
     recoverTime: 2000,
-  },
-  ],
+  }],
   ['cursor-effects', {
     size: 2, // size of the particle, default: 2
-    shape: ['star' | 'circle'], // shape of the particle, default: 'star'
+    shape: ['star'], // shape of the particle, default: 'star'
     zIndex: 999999999, // z-index property of the canvas, default: 999999999
-  },
-  ],
+  }],
   ['@vuepress-reco/vuepress-plugin-rss', {
     //RSS
     site_url: "https://alancorn.github.io",
